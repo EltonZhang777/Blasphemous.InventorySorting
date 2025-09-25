febf622a5bc177df6b7cd6a442233821cd0a9453
EltonZhang777 <280903158@qq.com>
2025-09-25 16:15:01 -0400

    Initial commit

---
+++ Blasphemous.InventorySorting/InventorySorting.cs
@@ -0,0 +1,14 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.InventorySorting
+{
+    public class InventorySorting : BlasMod
+    {
+        internal InventorySorting() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+        protected override void OnInitialize()
+        {
+            // Perform initialization here
+        }
+    }
+}
---
+++ Blasphemous.InventorySorting/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace Blasphemous.InventorySorting
+{
+    [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+    [BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+    internal class Main : BaseUnityPlugin
+    {
+        public static InventorySorting InventorySorting { get; private set; }
+
+        private void Start()
+        {
+            InventorySorting = new InventorySorting();
+        }
+    }
+}
